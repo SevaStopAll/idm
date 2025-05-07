b9b79003cc40186f66e49afe64c441c75893056d
vsevolod <ososkov_va@nlmk>
2025-05-07 11:05:33 +0300

    #505417 #542543 | change db scripts

--- migrations/init.sql
+++ migrations/init.sql
@@ -1,23 +1,35 @@
+-- +goose Up
+-- +goose StatementBegin
 CREATE TABLE employee
 (
-    id         bigint,
+    id         bigint GENERATED ALWAYS AS IDENTITY,
     name       text,
     created_at timestamptz,
     updated_at timestamptz
-)
+);
 
 CREATE TABLE role
 (
-    id         bigint,
+    id         bigint GENERATED ALWAYS AS IDENTITY,
     name       text,
     created_at timestamptz,
     updated_at timestamptz
-)
+);
 
 CREATE TABLE employee_role
 (
-    id          bigint,
-    employee_id bigint references employee.id,
-    role_id     bigint references role.id,
-    created_at timestamptz,
-)
+    id          bigint GENERATED ALWAYS AS IDENTITY,
+    employee_id bigint,
+    role_id     bigint,
+    created_at timestamptz
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE employee_role;
+
+DROP TABLE role;
+
+DROP TABLE employee;
+-- +goose StatementEnd
