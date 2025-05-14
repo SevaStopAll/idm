5f35a59d020fd79a12e99f2d15c30b66edc7b03b
vsevolod <ososkov_va@nlmk>
2025-05-14 12:01:25 +0300

    #505421 | change role tests because of linter warnings V2

--- inner/role/repository_test.go
+++ inner/role/repository_test.go
@@ -1,6 +1,5 @@
 package role
 
-//
 //import (
 //	"github.com/stretchr/testify/assert"
 //	"idm/inner/database"
@@ -20,10 +19,12 @@ package role
 //func Test_FindById(t *testing.T) {
 //	roleRepository := NewRoleRepository(database.ConnectDb())
 //	roleRepository.ExecuteQuery("ALTER SEQUENCE role_id_seq RESTART WITH 1")
-//	roleRepository.Save(RoleEntity{Name: "testRole"})
-//	res, err := roleRepository.FindById(1)
-//	if err == nil {
-//		assert.Equal(t, "testRole", res.Name)
+//	_, err2 := roleRepository.Save(RoleEntity{Name: "testRole"})
+//	if err2 == nil {
+//		res, err := roleRepository.FindById(1)
+//		if err == nil {
+//			assert.Equal(t, "testRole", res.Name)
+//		}
 //	}
 //	roleRepository.ExecuteQuery("DELETE FROM role")
 //}
@@ -31,29 +32,34 @@ package role
 //func Test_FindByIds(t *testing.T) {
 //	roleRepository := NewRoleRepository(database.ConnectDb())
 //	roleRepository.ExecuteQuery("ALTER SEQUENCE role_id_seq RESTART WITH 1")
-//	roleRepository.Save(RoleEntity{Name: "testRole1"})
-//	roleRepository.Save(RoleEntity{Name: "testRole2"})
-//	roleRepository.Save(RoleEntity{Name: "testRole3"})
-//	roleRepository.Save(RoleEntity{Name: "testRole4"})
-//	ids := make([]int64, 4)
-//	ids[0], ids[1], ids[2], ids[3] = 1, 2, 3, 4
-//	res, err := roleRepository.FindByIds(ids)
-//	if err != nil {
-//		assert.Equal(t, 4, len(res))
+//	firstId, err2 := roleRepository.Save(RoleEntity{Name: "testRole1"})
+//	secondId, err3 := roleRepository.Save(RoleEntity{Name: "testRole2"})
+//	thirdId, err4 := roleRepository.Save(RoleEntity{Name: "testRole3"})
+//	fourthId, err5 := roleRepository.Save(RoleEntity{Name: "testRole4"})
+//	if err2 == nil && err3 == nil && err4 == nil && err5 == nil {
+//		ids := make([]int64, 4)
+//		ids[0], ids[1], ids[2], ids[3] = firstId, secondId, thirdId, fourthId
+//		res, err := roleRepository.FindByIds(ids)
+//		if err != nil {
+//			assert.Equal(t, 4, len(res))
+//		}
 //	}
+//
 //	roleRepository.ExecuteQuery("DELETE FROM role")
 //}
 //
 //func Test_FindAll(t *testing.T) {
 //	roleRepository := NewRoleRepository(database.ConnectDb())
 //	roleRepository.ExecuteQuery("ALTER SEQUENCE role_id_seq RESTART WITH 1")
-//	roleRepository.Save(RoleEntity{Name: "testRole1"})
-//	roleRepository.Save(RoleEntity{Name: "testRole2"})
-//	roleRepository.Save(RoleEntity{Name: "testRole3"})
-//	roleRepository.Save(RoleEntity{Name: "testRole4"})
-//	res, err := roleRepository.FindAll()
-//	if err != nil {
-//		assert.Equal(t, 4, len(res))
+//	_, err2 := roleRepository.Save(RoleEntity{Name: "testRole1"})
+//	_, err3 := roleRepository.Save(RoleEntity{Name: "testRole2"})
+//	_, err4 := roleRepository.Save(RoleEntity{Name: "testRole3"})
+//	_, err5 := roleRepository.Save(RoleEntity{Name: "testRole4"})
+//	if err2 == nil && err3 == nil && err4 == nil && err5 == nil {
+//		res, err := roleRepository.FindAll()
+//		if err != nil {
+//			assert.Equal(t, 4, len(res))
+//		}
 //	}
 //	roleRepository.ExecuteQuery("DELETE FROM role")
 //}
@@ -61,39 +67,47 @@ package role
 //func Test_DeleteById(t *testing.T) {
 //	roleRepository := NewRoleRepository(database.ConnectDb())
 //	roleRepository.ExecuteQuery("ALTER SEQUENCE role_id_seq RESTART WITH 1")
-//	roleRepository.Save(RoleEntity{Name: "testRole1"})
-//	roleRepository.Save(RoleEntity{Name: "testRole2"})
-//	roleRepository.Save(RoleEntity{Name: "testRole3"})
-//	roleRepository.Save(RoleEntity{Name: "testRole4"})
-//	res, err := roleRepository.FindAll()
-//	if err == nil {
-//		assert.Equal(t, 4, len(res))
+//	_, err2 := roleRepository.Save(RoleEntity{Name: "testRole1"})
+//	_, err3 := roleRepository.Save(RoleEntity{Name: "testRole2"})
+//	_, err4 := roleRepository.Save(RoleEntity{Name: "testRole3"})
+//	_, err5 := roleRepository.Save(RoleEntity{Name: "testRole4"})
+//	if err2 == nil && err3 == nil && err4 == nil && err5 == nil {
+//		res, err := roleRepository.FindAll()
+//		if err == nil {
+//			assert.Equal(t, 4, len(res))
+//		}
+//		err = roleRepository.DeleteById(1)
+//		if err != nil {
+//			res, err = roleRepository.FindAll()
+//			if err == nil {
+//				assert.Equal(t, 3, len(res))
+//			}
+//		}
+//		roleRepository.ExecuteQuery("DELETE FROM role")
 //	}
-//	roleRepository.DeleteById(1)
-//	res, err = roleRepository.FindAll()
-//	if err == nil {
-//		assert.Equal(t, 3, len(res))
-//	}
-//	roleRepository.ExecuteQuery("DELETE FROM role")
 //}
 //
 //func Test_DeleteByIds(t *testing.T) {
 //	roleRepository := NewRoleRepository(database.ConnectDb())
 //	roleRepository.ExecuteQuery("ALTER SEQUENCE role_id_seq RESTART WITH 1")
-//	roleRepository.Save(RoleEntity{Name: "testRole1"})
-//	roleRepository.Save(RoleEntity{Name: "testRole2"})
-//	roleRepository.Save(RoleEntity{Name: "testRole3"})
-//	roleRepository.Save(RoleEntity{Name: "testRole4"})
-//	res, err := roleRepository.FindAll()
-//	if err == nil {
-//		assert.Equal(t, 4, len(res))
-//	}
-//	ids := make([]int64, 2)
-//	ids[0], ids[1] = 1, 2
-//	roleRepository.DeleteByIds(ids)
-//	res, err = roleRepository.FindAll()
-//	if err == nil {
-//		assert.Equal(t, 2, len(res))
+//	_, err2 := roleRepository.Save(RoleEntity{Name: "testRole1"})
+//	_, err3 := roleRepository.Save(RoleEntity{Name: "testRole2"})
+//	_, err4 := roleRepository.Save(RoleEntity{Name: "testRole3"})
+//	_, err5 := roleRepository.Save(RoleEntity{Name: "testRole4"})
+//	if err2 == nil && err3 == nil && err4 == nil && err5 == nil {
+//		res, err := roleRepository.FindAll()
+//		if err == nil {
+//			assert.Equal(t, 4, len(res))
+//		}
+//		ids := make([]int64, 2)
+//		ids[0], ids[1] = 1, 2
+//		err = roleRepository.DeleteByIds(ids)
+//		if err == nil {
+//			res, err = roleRepository.FindAll()
+//			if err == nil {
+//				assert.Equal(t, 2, len(res))
+//			}
+//		}
 //	}
 //	roleRepository.ExecuteQuery("DELETE FROM role")
 //}
